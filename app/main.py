2fed5cf662069a3a9e55bb89cd9805f18706f036
Mahendra Patel <mpatel468@hotmail.co.uk>
2021-12-31 12:50:26 +0530

    Added CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 # ./app/main.py
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from . import models
 from .db import engine
 from app.routers import todo, user, auth
@@ -10,10 +11,31 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(todo.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return """
+<!Doctype html>
+    <html>
+        <body>
+            <h1>Todo API</h1>
+            <div class="btn-group">
+                <a href="/docs"><button>SwaggerUI</button></a>
+                <a href="/redoc"><button>Redoc</button></a>
+            </div>
+        </body>
+    </html>
+"""
